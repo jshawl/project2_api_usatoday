4e0e5139aaf7ee050592f8b9d02c343e6475305b
Haleigh Harper <haleighharper@Haleighs-MacBook-Pro.local>
2015-04-21 14:36:15 -0400

    added flickraw gem

--- geo_pic/app/controllers/welcome_controller.rb
+++ geo_pic/app/controllers/welcome_controller.rb
@@ -3,11 +3,11 @@ class WelcomeController < ApplicationController
   def index
   end
 
-  def search
-    search_array = params[:search].split(' ')
-    search_array.join('+')
-    @flikr_search = HTTParty.get("https://api.flickr.com/services/rest/?method=flickr.places.find&api_key=e81306643b01d75c37c5e14f3d0ca1ab&query=#{params[:search]}&format=rest&api_sig=4d135b550bd716353d4ec0c07f953e19")
-  end
+  # def search
+  #   search_array = params[:search].split(' ')
+  #   search_array.join('+')
+  #   @flikr_search = HTTParty.get("https://api.flickr.com/services/rest/?method=flickr.places.find&api_key=e81306643b01d75c37c5e14f3d0ca1ab&query=#{params[:search]}&format=rest&api_sig=4d135b550bd716353d4ec0c07f953e19")
+  # end
 
 
 end
