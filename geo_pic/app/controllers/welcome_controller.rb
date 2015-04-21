bcb54dfdd2e9e05947478b8b39d98e7db52e7711
Haleigh Harper <haleighharper@Haleighs-MacBook-Pro.local>
2015-04-21 16:41:11 -0400

    updating api access

--- geo_pic/app/controllers/welcome_controller.rb
+++ geo_pic/app/controllers/welcome_controller.rb
@@ -6,19 +6,28 @@ class WelcomeController < ApplicationController
   def index
   end
 
-  def search
+  # def search
 
 
     # FlickRaw.api_key="04984bb3307b34805b31d3230a5c5b31"
     # FlickRaw.shared_secret="5fc20f0dcb83e30d"
     # flickr.places.find(params[:search])
 
+  #
+  #
+  # @search = params[:search]
+  # @api_call= HTTParty.get("https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=04984bb3307b34805b31d3230a5c5b31&tags=#{@search}&format=rest")
 
 
+
+
+def search
   @search = params[:search]
-  @api_call = HTTParty.get("https://api.flickr.com/services/rest/?method=flickr.places.find&api_key=04984bb3307b34805b31d3230a5c5b31=#{@search}")
 
-
+    @farm_id = HTTParty.get("https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=04984bb3307b34805b31d3230a5c5b31#{@search}&format=json&nojsoncallback=1").parsed_response["photos"]["photo"]["farm"]
+    @server_id = HTTParty.get("https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=04984bb3307b34805b31d3230a5c5b31#{@search}&format=json&nojsoncallback=1").parsed_response["photos"]["photo"]["server"]
+    @id = HTTParty.get("https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=04984bb3307b34805b31d3230a5c5b31#{@search}&format=json&nojsoncallback=1").parsed_response["photos"]["photo"]["id"]
+    @secret = HTTParty.get("https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=04984bb3307b34805b31d3230a5c5b31#{@search}&format=json&nojsoncallback=1").parsed_response["photos"]["photo"]["secret"]
   end
 
 
