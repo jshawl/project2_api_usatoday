54144cd6f1f3024b2dd3d17e3700cb12478b6ec3
Haleigh Harper <haleighharper@Haleighs-MacBook-Pro.local>
2015-04-22 11:08:55 -0400

    figaro funcational

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -6,28 +6,13 @@ class WelcomeController < ApplicationController
   def index
   end
 
-  # def search
+  def search
 
+    secret_key = ENV['secret_key']
 
-    # FlickRaw.api_key="04984bb3307b34805b31d3230a5c5b31"
-    # FlickRaw.shared_secret="5fc20f0dcb83e30d"
-    # flickr.places.find(params[:search])
-
-  #
-  #
-  # @search = params[:search]
-  # @api_call= HTTParty.get("https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=04984bb3307b34805b31d3230a5c5b31&tags=#{@search}&format=rest")
-
-  secret_key = ENV['secret_key']
-
-
-
-
-def search
-  search_results = HTTParty.get("https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=04984bb3307b34805b31d3230a5c5b31&tags=#{params[:search]}&format=rest").parsed_response["rsp"]["photos"]["photo"]
-  @photos = search_results.first(20)
-end
-
+    search_results = HTTParty.get("https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=#{secret_key}&tags=#{params[:search]}&format=rest").parsed_response["rsp"]["photos"]["photo"]
+    @photos = search_results.first(20)
+  end
 
 
 end
