5047311553db6814292cdd7eac88702800640d5a
Haleigh Harper <haleighharper@Haleighs-MacBook-Pro.local>
2015-04-22 12:21:49 -0400

    fixing heroku

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -12,7 +12,7 @@ class WelcomeController < ApplicationController
 
     search_value = params[:search].gsub(/\s/,"%20")
 
-    search_results = HTTParty.get("https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=#{secret_key}&tags=#{params[:search]}&format=rest").parsed_response["rsp"]["photos"]["photo"]
+    search_results = HTTParty.get("https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=#{secret_key}&tags=#{search_value}&format=rest").parsed_response["rsp"]["photos"]["photo"]
 
     @photos = search_results.first(20)
 
