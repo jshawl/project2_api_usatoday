13a3cec3fd14624bc28c24de5b06a5fa293a7b43
Haleigh Harper <heharper4@gmail.com>
2015-04-23 17:28:57 -0400

    validation for empty search term

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -12,11 +12,12 @@ class WelcomeController < ApplicationController
 
     search_value = params[:search].gsub(/\s/,"%20")
 
-    search_results = HTTParty.get("https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=#{secret_key}&tags=#{search_value}&format=rest").parsed_response["rsp"]["photos"]["photo"]
-
-    @photos = search_results.first(100)
-
+    if search_value.present?
+      search_results = HTTParty.get("https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=#{secret_key}&tags=#{search_value}&format=rest").parsed_response["rsp"]["photos"]["photo"]
+      @photos = search_results.first(100)
+      render :search
+    else
+      redirect_to :back, notice: "Must provide a search term"
+    end
   end
-
-
 end
