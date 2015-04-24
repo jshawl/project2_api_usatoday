b8a00c8a409819d6895b1ccaf3be04a1c6dc6d2c
Jared Yamauchi <jared.yamauchi@gmail.com>
2015-04-24 10:17:14 -0400

    front-page update

--- db/schema.rb
+++ db/schema.rb
@@ -34,7 +34,6 @@ ActiveRecord::Schema.define(version: 20150423152542) do
     t.text     "hometown"
     t.text     "interests"
     t.text     "about_me"
-    t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
