5f5aa6bde558855b4ea3ab2b9280c33c346e91b4
Haleigh Harper <heharper4@gmail.com>
2015-04-24 13:23:24 -0400

    added back seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,15 +4,15 @@
 # Examples:
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-# #   Mayor.create(name: 'Emanuel', city: cities.first)
-Place.create(user_id: 3, location: "Washington D.C.")
-#
-#   # Photo.create(place_id: 1, flickr_id: "http://washingtondc.riverrunfororphans.org/Washington%20DC/assets/Image/washington_dc.jpg")
-#
-Place.create(user_id: 3, location: "New York")
-#
-#   # Photo.create(place_id: 2, flickr_id: "http://www.theworkingworld.org/images/NYSkyline.jpg")
-#
-Place.create(user_id: 3, location: "Kansas City")
+#   Mayor.create(name: 'Emanuel', city: cities.first)
+Place.create(user_id: 1, location: "Washington D.C.")
+
+  # Photo.create(place_id: 1, flickr_id: "http://washingtondc.riverrunfororphans.org/Washington%20DC/assets/Image/washington_dc.jpg")
+
+Place.create(user_id: 1, location: "New York")
+
+  # Photo.create(place_id: 2, flickr_id: "http://www.theworkingworld.org/images/NYSkyline.jpg")
+
+Place.create(user_id: 1, location: "Kansas City")
 
   # Photo.create(place_id: 3, flickr_id: "http://ynpn.org/kansascity/files/2010/10/new-kc-skyline.jpg")
